8d138c7e4c6016b2358919f8f47485a545935cfc
Kyle Law <kylelaw1121@gmail.com>
2025-03-01 12:37:48 +0800

    set image from the start

--- crash_back_loop/intro/background.sh
+++ crash_back_loop/intro/background.sh
@@ -1,2 +1,2 @@
-kubectl create deployment webapp --image=nginx
-kubectl set image deployment/webapp nginx=invalidimage:latest
+kubectl create deployment webapp --image=invalidimage:latest
+# kubectl set image deployment/webapp nginx=invalidimage:latest
